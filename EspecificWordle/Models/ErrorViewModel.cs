16de789d5a0bd311701dcf4c6d6a927e86fae2bd
Lautaro <lau_manzo12@hotmail.com>
2024-01-28 17:34:41 -0300

    Add project files.

---
+++ EspecificWordle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EspecificWordle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
