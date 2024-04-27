7500db386bc616560a08917d659c7efb85b71936
Lautaro <lau_manzo12@hotmail.com>
2024-04-27 17:21:32 -0300

    Se hacen unos ajustes para adaptar el teclado web a los inputs

--- EspecificWordle/wwwroot/js/site.js
+++ EspecificWordle/wwwroot/js/site.js
@@ -1,7 +1,14 @@
 ﻿
+// funcion para manejar el borrado en inputs
+function onDeleteClick() {
 
+    // Borro el valor del input
+    inputFocus.val("");
 
+    // Busco el input previo al borrado para pasarle el focus.
+    inputFocus.parent().prev('div').find('input').focus();
 
-
-
-
+    // Mantiene el foco si el input a borrar es el primero 
+    if ($('.divWordle > div:not(.div-disable)').find('input').first().is(inputFocus))
+        inputFocus.focus();
+}
